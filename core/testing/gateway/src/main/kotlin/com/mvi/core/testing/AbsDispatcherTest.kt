81e46c8f86c987f6fca4a7bdbbcfd8da59e26da1
droideveloper <fatih.sen.boun@gmail.com>
2026-05-26 16:20:54 +0300

    Add :core:testing kdoc documentations

--- core/testing/gateway/src/main/kotlin/com/mvi/core/testing/AbsDispatcherTest.kt
+++ core/testing/gateway/src/main/kotlin/com/mvi/core/testing/AbsDispatcherTest.kt
@@ -10,13 +10,48 @@ import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 
+/**
+ * Base test class for coroutines testing with unconfined test dispatchers.
+ *
+ * This class sets up the main dispatcher to use [UnconfinedTestDispatcher]
+ * which allows coroutines to be tested without the overhead of dispatchers.
+ * All child test classes should extend this class to get automatic dispatcher
+ * setup and teardown.
+ *
+ * ## Usage
+ * ```kotlin
+ * class MyFeatureTest : AbsDispatcherTest() {
+ *     @get:Rule val composeContentRule = createAndroidComposeRule<Activity>()
+ *
+ *     @Before
+ *     fun setup() {
+ *         composeContentRule.setContent { /* UI */ }
+ *     }
+ * }
+ * ```
+ *
+ * ## Threading
+ * Uses [UnconfinedTestDispatcher] which allows suspension points to be
+ * evaluated immediately in the current context. This is faster but requires
+ * careful testing to ensure proper completion.
+ *
+ * @see UnconfinedTestDispatcher
+ * @see ComposeContentTestRule
+ */
 abstract class AbsDispatcherTest {
 
+    /**
+     * Sets up the main dispatcher to use [UnconfinedTestDispatcher]
+     * before each test execution.
+     */
     @Before
     fun setup() {
         Dispatchers.setMain(UnconfinedTestDispatcher())
     }
 
+    /**
+     * Resets the main dispatcher to its original state after each test.
+     */
     @After
     fun tearDown() {
         Dispatchers.resetMain()
--- core/testing/implementation/src/main/kotlin/com/mvi/core/testing/AbsAndroidUnitTest.kt
+++ core/testing/implementation/src/main/kotlin/com/mvi/core/testing/AbsAndroidUnitTest.kt
@@ -9,6 +9,38 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import org.junit.Rule
 import org.robolectric.annotation.Config
 
+/**
+ * Base test class for Compose UI testing with Robolectric.
+ *
+ * This class provides a convenient way to run Compose tests against multiple
+ * SDK versions. It sets up the Compose content rule and provides a helper
+ * [setScreen] extension function to easily set the test screen.
+ *
+ * ## Usage
+ * ```kotlin
+ * class MyComposableTest : AbsAndroidUnitTest() {
+ *
+ *     @Test
+ *     fun testMyComposable() {
+ *         with(testRule) {
+ *              setScreen {
+ *                  MyComposable()
+ *              }
+ *
+ *              onNodeWithText("Hello").isPresent
+ *         }
+ *     }
+ * }
+ * ```
+ *
+ * ## SDK Coverage
+ * Tests are configured to run against API 26 (Android 8.0 N) by default.
+ * Add additional SDK versions to the [Config.sdk] annotation as needed.
+ *
+ * @param sdk The Android SDK versions to test against
+ * @see ComposeContentTestRule
+ * @see ComposeTestRule
+ */
 @Config(
     sdk = [
         Build.VERSION_CODES.N,
@@ -16,9 +48,23 @@ import org.robolectric.annotation.Config
 )
 abstract class AbsAndroidUnitTest {
 
+    /**
+     * The Compose content test rule for running Compose UI tests.
+     *
+     * Override this in subclasses to use a different rule implementation
+     * or to provide a custom rule configuration.
+     */
     @get:Rule
     val testRule: ComposeTestRule = createAndroidComposeRule<ComponentActivity>()
 
+    /**
+     * Convenience extension function to set the Compose content on the test rule.
+     *
+     * This function automatically handles the different types of compose rules
+     * and applies the content to the appropriate rule implementation.
+     *
+     * @param content The Composable UI to set as the test screen
+     */
     fun ComposeTestRule.setScreen(
         content: @Composable () -> Unit,
     ) {
